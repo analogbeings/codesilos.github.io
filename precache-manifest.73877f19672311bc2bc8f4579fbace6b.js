5ba86dcd7764b92bb6a1b35237a1a2c7784c2c27
zowhair <zowhair@gmail.com>
2019-06-20 10:55:38 +0500

    Updates

---
+++ precache-manifest.73877f19672311bc2bc8f4579fbace6b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e415fa7617d2e67ea361",
+    "url": "/static/css/main.1784e711.chunk.css"
+  },
+  {
+    "revision": "e415fa7617d2e67ea361",
+    "url": "/static/js/main.e415fa76.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "742c11259e428c9d6080",
+    "url": "/static/js/2.742c1125.chunk.js"
+  },
+  {
+    "revision": "d56f2edf89cbdfed9adfde80ff7abdea",
+    "url": "/static/media/bart-logo.d56f2edf.png"
+  },
+  {
+    "revision": "0585128fe2ab089bab585e528fd9fc51",
+    "url": "/static/media/my_logo_1.0585128f.png"
+  },
+  {
+    "revision": "ac526b4ffe9105e54c4b5c3a73c70fb7",
+    "url": "/static/media/instagram_dark.ac526b4f.png"
+  },
+  {
+    "revision": "6026a1a4c584b8c56045628ceef5c30c",
+    "url": "/index.html"
+  }
+];
